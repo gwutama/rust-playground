3408f55ed5d29dd8eb0489e69dfcffed98a3b728
Galuh Utama <galuh.utama@gwutama.de>
2023-05-22 20:47:02 +0200

    Mutable, reference, borrowing

--- consts-vars/src/main.rs
+++ consts-vars/src/main.rs
@@ -20,4 +20,16 @@ fn main() {
     let score: &str = "You Win!";
 
     let run: MagicPower = 7800;
+
+    println!("Energy ref: {:p} {}", &energy, energy);
+
+    // references
+    let energy_ref = &mut energy;
+
+    // set value of energy_ref to 20
+    *energy_ref = 20; // error: cannot assign twice to immutable variable `energy_ref`
+
+    println!("Energy ref: {:p} {}", energy_ref, *energy_ref);
+    energy = 42;
+    println!("Energy ref: {:p} {}", &energy, energy);
 }
