f9a9ad1f0723b9039f2edecc4e763001a4d5780c
Galuh Utama <galuh.utama@gwutama.de>
2023-05-24 10:40:50 +0200

    Tuples, structs, enums, results, options

--- data-structures/src/main.rs
+++ data-structures/src/main.rs
@@ -41,7 +41,74 @@ fn main() {
     let pop: Option<i32> = vec1.pop();
     println!("vec1 {:?}, pop {}", vec1, pop.unwrap_or_default());
 
+    let chars: Vec<char> = foo.chars().collect();
+
+    for c in chars.iter() {
+        println!("{}", c);
+    }
+
     // Slices
     let slice = &vec2[1..3];
     println!("slice {:?}", slice);
+
+    // Tuples
+    let thor = ("Thor", true, 3500);
+    println!("{:?}", thor);
+
+    println!("{}'s power is {}", thor.0, thor.2);
+    let (name, _, power) = thor;
+    println!("{}'s power is {}", name, power);
+
+    // Structs
+    let mut player1 = Player {
+        name: String::from("Player_1"),
+        health: 100,
+        level: 1,
+        score: 0,
+    };
+    player1.health = 90;
+    println!("{} {} {} {}", player1.name, player1.health, player1.level, player1.score);
+
+    // Enums
+    enum Direction {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+
+    let player_direction: Direction = Direction::Down;
+
+    // Result
+    let result_ok = get_result_ok();
+    let result_error = get_result_error();
+    println!("{:?} {:?}", result_ok, result_error);
+
+    // Option
+    let option_some = get_option_some();
+    let option_none = get_option_none();
+    println!("{:?} {:?}", option_some, option_none);
+}
+
+fn get_result_ok() -> Result<bool, String> {
+    return Ok(true);
+}
+
+fn get_result_error() -> Result<bool, String> {
+    return Err(String::from("Error"));
+}
+
+fn get_option_some() -> Option<String> {
+    return Some(String::from("Hello"));
+}
+
+fn get_option_none() -> Option<String> {
+    return None;
+}
+
+struct Player {
+    name: String,
+    health: i32,
+    level: u8,
+    score: u64,
 }
