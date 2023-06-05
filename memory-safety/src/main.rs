7c1bd9fa793e32b86e98e36342466965b2ee1277
Galuh Utama <galuh.utama@gwutama.de>
2023-06-05 17:05:09 +0200

    more pointers

--- memory-safety/src/main.rs
+++ memory-safety/src/main.rs
@@ -17,13 +17,31 @@ fn main() {
     // pointers
     let m: i32 = 5;
     let n: &i32 = &m; // a pointer that borrows m
+    let o: &i32 = &m; // can borrow. Multiple immutable borrows are allowed
     println!("The address of m is {:p} and n is {:p}", &m, &n);
     println!("The value of m is {} and n is {} or {}", m, n, *n);
     println!("{}", square(&m)); // &m because m is not a pointer and square expects a pointer
     println!("{}", square(n)); // n is a pointer and square expects a pointer
     println!("{}", square(&n)); // this works too!
+
+    let mut p: i32 = 5;
+    println!("The value of p is {}", p);
+    let mut q: &mut i32 = &mut p; // a mutable pointer that borrows p
+    // let mut r: &mut i32 = &mut p; // cannot borrow. Only one mutable borrow is allowed
+    println!("The value of q is {} or {}", q, *q);
+    *q = 10;
+    println!("The value of q is {} or {}", q, *q);
+    println!("The value of p is {}", p);
+    p = 20;
+    println!("The value of p is {}", p);
+    add_ten(&mut p);
+    println!("The value of p is {}", p); // p is now 30
 }
 
 fn square(x: &i32) -> i32 {
     return x * x;
+}
+
+fn add_ten(x: &mut i32) {
+    *x += 10;
 }
